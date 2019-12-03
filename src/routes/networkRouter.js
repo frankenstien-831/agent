aae6845d2d7e7a0b87364ffd4c02323510a942c8
MarcusJones <jones.0bj3@gmail.com>
2019-12-03 15:29:13 +0100

    refactor move ocean to /models

--- src/routes/networkRouter.js
+++ src/routes/networkRouter.js
@@ -6,24 +6,24 @@ const router = express.Router();
 // POST request to register new network
 router.get(
   "/aquarius/status",
-  
+
   async (req, res, next) => {
-	
-	var networkname=await res.locals.ocean.aquarius.getVersionInfo(); 
-	
-	res.status(201).json(networkname);
- }   
+
+	var networkname=await res.locals.ocean.aquarius.getVersionInfo();
+
+	res.status(200).json(networkname);
+ }
 );
 
 
 router.get(
   "/brizo/status",
-  
+
   async (req, res, next) => {
-	var networkname=await res.locals.ocean.brizo.getVersionInfo(); 
-	
-	res.status(201).json(networkname);
- }   
+	var networkname=await res.locals.ocean.brizo.getVersionInfo();
+
+	res.status(200).json(networkname);
+ }
 );
 
 
