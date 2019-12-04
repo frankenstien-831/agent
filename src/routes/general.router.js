1afdd4f581a3bcc27e34954ce89a97f1aef6ad74
MarcusJones <jones.0bj3@gmail.com>
2019-12-04 12:55:22 +0100

    update

--- src/routes/general.router.js
+++ src/routes/general.router.js
@@ -157,88 +157,88 @@ router.get(
 
 router.post("/consume",
     async (req, res, next) => {
-        // res.status(301).json("Refactored to /assets");
-            var responseObj = Array();
-            const accounts = await res.locals.ocean.accounts.list()
-            var did = req.body.did;
-            const consumeAsset = await res.locals.ocean.assets.resolve(did);
-            const service = consumeAsset.findServiceByType('Access')
-            const agreementId = await res.locals.ocean.assets.order(
-                consumeAsset.id,
-                service.serviceDefinitionId,
-                accounts[0]
+        res.status(301).json("Refactored to /assets");
+        var responseObj = Array();
+        const accounts = await res.locals.ocean.accounts.list()
+        var did = req.body.did;
+        const consumeAsset = await res.locals.ocean.assets.resolve(did);
+        const service = consumeAsset.findServiceByType('Access')
+        const agreementId = await res.locals.ocean.assets.order(
+            consumeAsset.id,
+            service.serviceDefinitionId,
+            accounts[0]
+        )
+        var index = -1;
+        var consumerAccount = accounts[0]
+        var resultPath = './downloads/';
+        var useSecretStore = 0;
+        const ddo = consumeAsset;
+        const { metadata } = ddo.findServiceByType('Metadata')
+
+        const accessService = ddo.findServiceById(service.serviceDefinitionId)
+
+        const { files } = metadata.base
+
+        const { serviceEndpoint } = accessService
+
+        if (!serviceEndpoint) {
+            throw new Error(
+                'Consume asset failed, service definition is missing the `serviceEndpoint`.'
             )
-            var index = -1;
-            var consumerAccount = accounts[0]
-            var resultPath = './downloads/';
-            var useSecretStore = 0;
-            const ddo = consumeAsset;
-            const { metadata } = ddo.findServiceByType('Metadata')
+        }
+        const signature =
+            (await consumerAccount.getToken()) ||
+            (await res.locals.ocean.utils.signature.signText(
+                noZeroX(agreementId),
+                consumerAccount.getId()
+            ))
 
-            const accessService = ddo.findServiceById(service.serviceDefinitionId)
+        for (const fileitem of files) {
 
-            const { files } = metadata.base
+            var newfile = new Object();
+            newfile.ContentType = fileitem.ContentType;
+            newfile.index = fileitem.index;
+            newfile.contentLength = fileitem.contentLength;
+            newfile.compression = fileitem.compression;
+            let consumeUrl = serviceEndpoint
+            consumeUrl += `?index=${fileitem.index}`
+            consumeUrl += `&serviceAgreementId=${noZeroX(agreementId)}`
+            consumeUrl += `&consumerAddress=${consumerAccount.getId()}`
+            consumeUrl += `&signature=${signature}`
 
-            const { serviceEndpoint } = accessService
-
-            if (!serviceEndpoint) {
-                throw new Error(
-                    'Consume asset failed, service definition is missing the `serviceEndpoint`.'
-                )
-            }
-            const signature =
-                (await consumerAccount.getToken()) ||
-                (await res.locals.ocean.utils.signature.signText(
-                    noZeroX(agreementId),
-                    consumerAccount.getId()
-                ))
-
-            for (const fileitem of files) {
-
-                var newfile = new Object();
-                newfile.ContentType = fileitem.ContentType;
-                newfile.index = fileitem.index;
-                newfile.contentLength = fileitem.contentLength;
-                newfile.compression = fileitem.compression;
-                let consumeUrl = serviceEndpoint
-                consumeUrl += `?index=${fileitem.index}`
-                consumeUrl += `&serviceAgreementId=${noZeroX(agreementId)}`
-                consumeUrl += `&consumerAddress=${consumerAccount.getId()}`
-                consumeUrl += `&signature=${signature}`
-
-                try {
-                    const response = await fetch(consumeUrl, {
-                        method: 'GET',
-                        headers: {
-                            'Content-type': 'application/json'
-                        }
-                    })
-                    if (!response.ok) {
-                        throw new Error('Response error.')
+            try {
+                const response = await fetch(consumeUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json'
                     }
-                    let filename;
-                    try {
-                        filename = response.headers.get('content-disposition').match(/attachment;filename=(.+)/)[1];
-                    } catch (e) {
-                        try {
-                            filename = url.split('/').pop()
-                        } catch (e) {
-                            filename = `file${index}`
-                        }
-                    }
-                    newfile.filename = filename;
-                    newfile.data = b64enc.encode(await response.arrayBuffer())
-                    responseObj.push(newfile);
-
-
-                } catch (e) {
-                    console.error('Error consuming assets')
-                    console.error(e)
-                    throw e
+                })
+                if (!response.ok) {
+                    throw new Error('Response error.')
                 }
+                let filename;
+                try {
+                    filename = response.headers.get('content-disposition').match(/attachment;filename=(.+)/)[1];
+                } catch (e) {
+                    try {
+                        filename = url.split('/').pop()
+                    } catch (e) {
+                        filename = `file${index}`
+                    }
+                }
+                newfile.filename = filename;
+                newfile.data = b64enc.encode(await response.arrayBuffer())
+                responseObj.push(newfile);
+
+
+            } catch (e) {
+                console.error('Error consuming assets')
+                console.error(e)
+                throw e
             }
-            console.log('Files consumed')
-            res.status(200).json(responseObj);
+        }
+        console.log('Files consumed')
+        res.status(200).json(responseObj);
     }
 );
 
