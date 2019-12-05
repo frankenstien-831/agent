ca725a1b97cee28ba20629ca9ea8a4adc8420764
Matthias Kretschmann <m@kretschmann.io>
2019-12-05 14:56:44 +0100

    update to DDO schema based on OEP-8 v0.4

--- src/models/asset.js
+++ src/models/asset.js
@@ -1,24 +1,22 @@
 const AssetModel = {
-  assetId: null,
-  publisherId: null,
-
   // OEP-08 Attributes
   // https://github.com/oceanprotocol/OEPs/tree/master/8
-  base: {
-    name: null,
-    description: null,
-    dateCreated: null,
-    author: null,
-    type: '',
-    license: null,
-    copyrightHolder: null,
-    workExample: '',
-    files: [],
+  main: {
+    type: 'dataset',
+    name: '',
+    dateCreated: '',
+    author: '',
+    license: '',
+    price: '',
+    files: []
+  },
+  additionalInformation: {
+    description: '',
+    copyrightHolder: '',
     categories: [],
-    links: [],
     inLanguage: '',
     tags: [],
-    price: ''
+    workExample: ''
   }
 }
 
